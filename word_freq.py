d24a6cc2197c440eaf6efea5607d2651cc022827
Raloooo <optimuslove0223@icloud.com>
2022-04-12 20:15:49 +0900

    (1) new add

---
+++ word_freq.py
@@ -0,0 +1,19 @@
+import sys
+import re
+print(sys.argv)
+filename = sys.argv[1]
+n = int(sys.argv[2])
+
+c=0
+file = open(filename, mode = 'r')
+lines = file.readlines()
+
+dic = dict()
+for line in lines:
+	line = line.strip()
+	new_line = re.sub(r"[^a-zA-Z0-9]"," ", line)
+	real_new_line = new_line.split()
+	for name in real_new_line:
+		dic[name] = dic.get(name,0) + 1
+
+print(dic)
